87c3c77a172eba7951e0d057c741064ff04cd8c6
Ni236630 <nicholas.douso@gmail.com>
2020-11-17 11:56:01 -0600

    alter format slightly to be more readable. will alter once more

--- scripts/FishDataProvider.js
+++ scripts/FishDataProvider.js
@@ -1,20 +1,23 @@
-const fishCollection = [{
-    name: "Bart",
-    food: "crustaceans",
-    species: "",
-    length: "",
-    location: "",
+const fishCollection = [
 
-}, {
-    name: "Pinky",
-    food: "Small Fish",
-    species: "Sockeye Salmon",
-    length: "44",
-    location: "Anchorage, AK",
-}, {
-    name: "My Boy Blue",
-    food: "Fish",
-    species: "Bluefin Tuna",
-    length: "108",
-    location: "Gloucester, MA",
-}]
+    {
+        name: "Bart",
+        food: "crustaceans",
+        species: "",
+        length: "",
+        location: "",
+
+    }, {
+        name: "Pinky",
+        food: "Small Fish",
+        species: "Sockeye Salmon",
+        length: "44",
+        location: "Anchorage, AK",
+    }, {
+        name: "My Boy Blue",
+        food: "Fish",
+        species: "Bluefin Tuna",
+        length: "108",
+        location: "Gloucester, MA",
+    }
+]
