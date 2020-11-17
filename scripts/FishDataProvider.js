e9d65d993139a40d9cf2d9a5083ad66b6a7b2416
Ni236630 <nicholas.douso@gmail.com>
2020-11-17 16:48:55 -0600

    adding all js functionality

--- scripts/FishDataProvider.js
+++ scripts/FishDataProvider.js
@@ -1,25 +1,34 @@
 const fishCollection = [
 
     {
+        image: "salmon.jpg",
         name: "Bart",
-        food: "crustaceans",
-        species: "",
-        length: "",
-        location: "",
+        species: "Rock Fish",
+        length: 10,
+        location: "Carribean",
+        food: "crustaceans"
+
 
     },
     {
+        image: "salmon.jpg",
         name: "Pinky",
-        food: "Small Fish",
         species: "Sockeye Salmon",
-        length: "44",
+        length: 44,
         location: "Anchorage, AK",
+        food: "Small Fish"
     },
     {
+        image: "tuna-steak.jpg",
         name: "My Boy Blue",
-        food: "Fish",
         species: "Bluefin Tuna",
-        length: "108",
+        length: 108,
         location: "Gloucester, MA",
+        food: "Fish"
     }
 ]
+
+
+export const useFish = () => {
+    return fishCollection.slice()
+}
