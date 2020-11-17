22b711a74837879404bf4d979ff28004ad08ff71
Ni236630 <nicholas.douso@gmail.com>
2020-11-17 11:48:25 -0600

    created fish object data

---
+++ scripts/FishDataProvider.js
@@ -0,0 +1,20 @@
+const fishCollection = [{
+    name: "Bart",
+    food: "crustaceans",
+    species: "",
+    length: "",
+    location: "",
+
+}, {
+    name: "Pinky",
+    food: "Small Fish",
+    species: "Sockeye Salmon",
+    length: "44",
+    location: "Anchorage, AK",
+}, {
+    name: "My Boy Blue",
+    food: "Fish",
+    species: "Bluefin Tuna",
+    length: "108",
+    location: "Gloucester, MA",
+}]
